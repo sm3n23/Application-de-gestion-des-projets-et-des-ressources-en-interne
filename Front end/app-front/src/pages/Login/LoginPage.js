abd0f56a1a683b732d06a560e9360432a65ec86c
sm3n23 <121973133+sm3n23@users.noreply.github.com>
2024-06-19 18:00:38 +0100

    pagination and login

--- Front end/app-front/src/pages/Login/LoginPage.js
+++ Front end/app-front/src/pages/Login/LoginPage.js
@@ -1,14 +1,24 @@
 // src/pages/Login/LoginPage.js
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "./LoginFunction";
 import { AuthContext } from "../../context/AuthContext";
+import './Login.css';
 
 const LoginPage = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login: authLogin } = useContext(AuthContext);
   const navigate = useNavigate();
+  useEffect(() => {
+    // Apply custom styles to the body for the login page
+    document.body.classList.add('custom-login-body');
+
+    // Cleanup function to remove the custom styles when the component is unmounted
+    return () => {
+      document.body.classList.remove('custom-login-body');
+    };
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,26 +33,43 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
+    <div className="custom-login-background">
+      <img src="/images/bplogo.png" alt="Company Logo" className="custom-login-logo" />
+      <div className="custom-login-text">
+        <h1>Heureux de vous voir !</h1>
+        <p>Connectez-vous ou créez votre compte...</p>
+      </div>
+      <div className="custom-login-container">
+        <div className="custom-login-section">
+          
+        <form className="custom-login-form" onSubmit={handleSubmit}>
+          
+            <label htmlFor="email">Username *</label>
+            <input type="text"
+              className="text-input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Entrez votre email" required />
+            
+            <label htmlFor="password">Mot de passe *</label>
+            <input type="password"
+            className="text-input"
+             id="password" name="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
+            placeholder="Entrez votre mot de passe" required />
+            
+            <div className="custom-login-options">
+              <label className="box-label"><input type="checkbox" name="stay-connected" className="box-input" />
+              <span className="box-label">Rester connectée</span> </label>
+            </div>
+            
+            <button type="submit" className="custom-login-btn">CONNEXION</button>
+          
+        </form>
+      </div>
+    </div>
+      
     </div>
   );
 };
